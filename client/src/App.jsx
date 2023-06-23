14aaade8d33b2c43a82ae507e809ed358b014b4e
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-06-23 11:28:55 +0530

    basic setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>UsTime</h1>
-    </div>
-  );
+  return <div className="App">
+  <h1>hello</h1>
+  </div>;
 }
 
 export default App;
